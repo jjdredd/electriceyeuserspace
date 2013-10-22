4e48adc3f284a4cd53b30578e4c2a223048b7858
Judge_Dredd <the.guard@mail.ru>
2013-10-22 21:52:12 +0400

    fixes for conversion

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <windows.h>
 #include "init.c"
+#include "mkimg.c"
 
 int main(){
     HANDLE hFile;
     DWORD dwReturn = 0;
-    char    *buff;
+    char    *buff, *buff_rgb_conv;
     FILE    *fd;
-    int i, payload[2], w, h, d, buffsz;
+    int i, payload[2], w, h, d, buffsz, k;
     HDC ScreenDC;
 
     if( !(ScreenDC = GetDC(NULL)) ){
@@ -46,10 +47,33 @@ int main(){
         return -1;
     }
 */
-    fd = fopen("./out.rgb","wb+");
-    fwrite(buff, sizeof(char), buffsz, fd);
-
+    fd = fopen("./out.jpeg","wb+");
+    printf("converting and writing\n");
+    if( d == 4 ){
+            printf("removing a %i\n", buffsz*3/4);
+      if( (buff_rgb_conv = (char *)malloc(buffsz*3/4)) == 0 ){
+        printf("Error allocating\n");
+        return -1;
+      }
+      //bgra to rgb
+      for(i = 0, k = 0; i < buffsz; i += 4, k +=3 ){
+            buff_rgb_conv[k+2] = buff[i];
+            buff_rgb_conv[k+1] = buff[i+1];
+            buff_rgb_conv[k] = buff[i+2];
+        }
+      mkimg(buff_rgb_conv, w, h, fd);
+    }
+    else{
+     for(i = 0; i < buffsz; i += 3){
+        k = buff[i];
+        buff[i] = buff[i+2];
+        buff[i+2] = k;
+	 }
+     mkimg(buff, w, h, fd);
+    }
     fclose(fd);
+    free(buff);
+    if(buff_rgb_conv) free(buff_rgb_conv);
     return 0;
 
 }
